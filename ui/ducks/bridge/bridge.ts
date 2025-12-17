c78b32aae110874a0004d0e4c9cbfd2452d9a25f
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-12-17 09:53:40 -0800

    fix: assetId comparison

--- ui/ducks/bridge/bridge.ts
+++ ui/ducks/bridge/bridge.ts
@@ -97,14 +97,10 @@ const bridgeSlice = createSlice({
       state.fromTokenExchangeRate = null;
       // Unset toToken if it's the same as the fromToken
       if (
-        (state.fromToken?.assetId &&
-          state.toToken?.assetId &&
-          state.fromToken.assetId?.toLowerCase() ===
-            state.toToken.assetId?.toLowerCase()) ||
-        (state.fromToken?.address &&
-          state.toToken?.address &&
-          state.fromToken.address.toLowerCase() ===
-            state.toToken.address.toLowerCase())
+        state.fromToken?.assetId &&
+        state.toToken?.assetId &&
+        state.fromToken.assetId?.toLowerCase() ===
+          state.toToken.assetId?.toLowerCase()
       ) {
         state.toToken = null;
       }
